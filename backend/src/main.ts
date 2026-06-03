be93fafe227d0d6e0db1de81de68ceff6559f4b5
Dev <dev@local>
2026-06-04 07:13:25 +0800

    fix: remove duplicate api prefix, ensure TimePicker parse fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-  app.setGlobalPrefix('api');
   app.useBodyParser('json', { limit: '50mb' });
   await app.listen(3000);
 }
